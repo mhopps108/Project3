70429ba40934340100858f70cbaad721517619d8
mhopps108 <MHopps@Matts-Retina-MBP.local>
2015-05-08 03:45:00 -0700

    Initial Commit

---
+++ Project3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project3
+//
+//  Created by Matt Hopps on 5/8/15.
+//  Copyright (c) 2015 Matt Hopps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+    return 0;
+}
